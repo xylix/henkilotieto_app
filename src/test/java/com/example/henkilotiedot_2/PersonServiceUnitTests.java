50310e64c73735b121bb88f64991d0b289d8f734
xylix <kerk.pelt@gmail.com>
2023-10-20 16:02:44 +0300

    Update PersonServiceUnitTests to work

--- src/test/java/com/example/henkilotiedot_2/PersonServiceUnitTests.java
+++ src/test/java/com/example/henkilotiedot_2/PersonServiceUnitTests.java
@@ -1,6 +1,5 @@
 package com.example.henkilotiedot_2;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,31 +8,44 @@ import java.time.LocalDate;
 
 public class PersonServiceUnitTests {
     PersonService personService;
-    // This could be a BeforeAll but then we would need to set up the test class lifecycle
     @BeforeEach
     public void setUp() {
         this.personService = new PersonService();
     }
-    static Person createTestPerson() {
+    static Person createTestPerson(String name, String id) {
         LocalDate birth = LocalDate.of(1980, 10, 10);
         LocalDate death = null;
-        Person p = new Person(
-                "M Meikäläinen",
-                "101010-000",
+        return new Person(
+                name,
+                id,
                 "Testitie 1",
                 "Finnish",
                 "Suomi",
                 "Spouse",
                 birth,
                 death);
-        return p;
+    }
+
+    // Testing two methods in one test is a bad pattern, but we can't verify the creation without getting. And we can't
+    // Verify the getting without addition.
+    @Test
+    void addAndGetPerson() {
+        Person p = createTestPerson("M Meikäläinen", "101010-000");
+        this.personService.addPerson(p);
+        Person p2 = this.personService.getPerson("101010-000");
+        Assertions.assertEquals(p.id, p2.id);
+        Assertions.assertEquals(p, p2);
     }
 
     @Test
-    void getPerson() {
-        Person p = createTestPerson();
+    void addTwoPersons() {
+        Person p = createTestPerson("M Meikäläinen", "101010-000");
+        Person p2 = createTestPerson("Testi Testihenkilö", "000000-000");
         this.personService.addPerson(p);
-        Person p2 = this.personService.getPerson("101010-000");
-        Assertions.assertEquals(p, p2);
+        this.personService.addPerson(p2);
+
+        Assertions.assertEquals(p.name, this.personService.getPerson(p.id).name);
+        Assertions.assertEquals(p2.name, this.personService.getPerson(p2.id).name);
+
     }
 }
